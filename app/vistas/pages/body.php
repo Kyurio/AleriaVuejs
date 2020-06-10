eb59af6ed8ed639a44c62b910f0ef941d040a445
Kyurio <jose.naoi@hotmail.com>
2020-06-10 00:38:35 -0400

    nuevo inicio

--- app/vistas/pages/body.php
+++ app/vistas/pages/body.php
@@ -1,77 +1,36 @@
-<main role="main">
+<div id="app">
+  <div class="container">
 
-  <div class="container mt-5 py-2">
+    <div class="mt-5 mb-5 py-5">
+      <div class="row">
+        <div class="col-md-4">
 
-    <div class="row">
+          <div class="card">
+            <div class="view overlay">
+              <img class="card-img-top"
+              src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%28131%29.jpg" alt="Card image cap">
+              <a>
+                <div class="mask rgba-white-slight"></div>
+              </a>
+            </div>
+            <div class="card-body">
+              <h4 class="card-title">Card title</h4>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+                <a href="#!" class="black-text d-flex justify-content-end">
+                  <h5>Read more</h5>
+                </a>
+              </div>
+            </div>
 
-      <div class="col-sm-5 mt-5 content-title">
 
-        <div class="presentacion text-center animated bounceInLeft slow"><h1 class="title">DOUBLECODE</h1><h4 class="writing">&lt;Da el salto/&gt;</h4>
+
+          </div>
+
         </div>
       </div>
 
-      <div class="col-sm-7 mt-5 text-center content-body">
+    </div>
 
-        <div class="contenedor-cuerpo  animated bounceInRight slow">
-          <!--principal content-->
-
-          <a data-toggle="collapse" href="#servicios" role="button" aria-expanded="false" aria-controls="collapseExample"><h2>Servicios</i></h2></a>
-          <div class="collapse animated bounceInRight" id="servicios">
-
-            <div class="mt-1 mb-1 py-1">
-              <h5>Diseño y desarrollo web</h5>
-              <span class="badge">Diseño</span>
-              <span class="badge">Branding</span>
-              <span class="badge">Marketing</span>
-              <p>personalizados y adaptables,con posicionamiento SEO y estructuras precisas para la masificación de tu marca.</p>
-            </div>
-
-            <div class="mt-1 mb-1 py-1">
-              <h5>Software personalizado</h5>
-              <span class="badge">Desktop</span>
-              <span class="badge">Multiplaforma</span>
-              <span class="badge">Ux&Ui</span>
-              <p>Construcción de plataformas tecnológicas enfocadas en brindar funcionalidades que ayuden a fijar los objetivos de tu negocio.</p>
-            </div>
-
-            <div class="mt-1 mb-1 py-1">
-              <h5>Software de Automatización</h5>
-              <p>Desarrollamos Software de gestion y estadisticas basados en datos que te permitiran facilitar la toma inteligente de deciciones.</p>
-            </div>
-
-            <div class="mt-1 mb-1 py-1">
-              <h5>Administracion de redes sociales</h5>
-              <span class="badge">Twitter</span>
-              <span class="badge">Facebook</span>
-              <span class="badge">Instagram</span>
-              <p>Administramos tus redes sociales para que tu marca sea reconocida y viral.</p>
-            </div>
-
-
-
-          </div>
-          <!--quinto content precios-->
-          <!--seundo content-->
-          <a data-toggle="collapse" href="#proyectos" role="button" aria-expanded="false" aria-controls="collapseExample">
-            <h2>Nosotros</h2>
-          </a>
-
-          <div class="collapse animated bounceInRight" id="proyectos">
-            <p><b>Doublecode</b>tiene la misión de otorgar la mejor tecnología a los emprendedores del país y el mundo. Creamos software sofisticado y de alta escalabilidad que te permitira romper todos tus limites.</p>
-          </div>
-          <!--cuarto content-->
-
-          <a data-toggle="collapse" href="#contacto" role="button" aria-expanded="false" aria-controls="collapseExample"><h2>Contacto</h2></a>
-          <div class="collapse animated bounceInRight" id="contacto">
-            <br><a>
-              <p><i class="fa fa-map-marker"></i>Ovalle,Coquimbo,La serena-Chile</p></a><a href="https://wa.me/56965063392" target="_blank">
-                <p><i class="fab fa-whatsapp"></i>+569 65063392</p></a><a href="mailto:contacto@doublecode.cl">
-                  <p><i class="far fa-envelope"></i>contacto@doublecode.cl</p></a><a href="https://www.instagram.com/doublecode.cl/" target="_blank">
-                    <p><i class="fab fa-instagram"></i>Doublecode</p><a/>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
+  </div>
+</div>
